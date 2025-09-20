3a6d6b6f16fe6deb7d9436fe37afefe5e1d563ca
Pratham Darji <155282402+Pratham-Prog861@users.noreply.github.com>
2025-09-20 19:00:43 +0530

    add routes for comments, dashboard, healthcheck, likes, playlists, subscriptions, tweets, and videos with JWT verification

---
+++ src/routes/comment.routes.js
@@ -0,0 +1,21 @@
+import { Router } from 'express';
+import {
+    addComment,
+    deleteComment,
+    getVideoComments,
+    updateComment,
+    likeComment,
+    unlikeComment
+} from "../controllers/comment.controller.js"
+import {verifyJWT} from "../middlewares/auth.middleware.js"
+
+const router = Router();
+
+router.use(verifyJWT); // Apply verifyJWT middleware to all routes in this file
+
+router.route("/:videoId").get(getVideoComments).post(addComment);
+router.route("/c/:commentId").delete(deleteComment).patch(updateComment);
+router.route("/like/:commentId").post(likeComment);
+router.route("/unlike/:commentId").post(unlikeComment);
+
+export default router;
